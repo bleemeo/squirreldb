7c620d72ba6bcb4326a110e2c03f0542c0f01743
Pierre Fersing <pierre.fersing@bleemeo.com>
2021-03-17 16:30:53 +0100

    Use Redis in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -17,7 +17,14 @@ docker run --name squirreldb-test-cassandra -d -e MAX_HEAP_SIZE=128M -e HEAP_NEW
 docker exec squirreldb-test-cassandra cqlsh -e 'DROP KEYSPACE squirreldb_test' || true
 docker exec squirreldb-test-cassandra nodetool clearsnapshot || true
 
+echo
+echo "== Starting Redis"
+docker run --name squirreldb-test-redis -d redis:6.0.9 && sleep 5 || true
+docker exec squirreldb-test-redis redis-cli flushdb
+
+
 export SQUIRRELDB_CASSANDRA_ADDRESSES=$(docker inspect squirreldb-test-cassandra  -f '{{ .NetworkSettings.IPAddress }}'):9042
+export SQUIRRELDB_REDIS_ADDRESSES=$(docker inspect squirreldb-test-redis  -f '{{ .NetworkSettings.IPAddress }}'):6379
 export GORACE=halt_on_error=1
 
 echo
@@ -39,7 +46,7 @@ docker run --rm -u $UID -e HOME=/go/pkg \
 echo
 echo "== Running remote-storage-test"
 docker run --rm -u $UID -e HOME=/go/pkg \
-    -e SQUIRRELDB_CASSANDRA_ADDRESSES -e GORACE \
+    -e SQUIRRELDB_CASSANDRA_ADDRESSES -e GORACE -e SQUIRRELDB_REDIS_ADDRESSES \
     -e SQUIRRELDB_CASSANDRA_KEYSPACE=squirreldb_test \
     -v $(pwd):/src -w /src ${GO_MOUNT_CACHE} \
     --entrypoint '' \
