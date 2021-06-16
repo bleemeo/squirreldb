5ed26ca7c25cdff7672f8cadd47f255d105c304f
florian gabon <florian.gabon@bleemeo.com>
2021-06-16 11:07:47 +0200

    Add, telemetry

---
+++ telemetry/telemetry.go
@@ -0,0 +1,85 @@
+// Copyright 2015-2019 Bleemeo
+//
+// bleemeo.com an infrastructure monitoring solution in the Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package telemetry
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Telemetry struct {
+	ID string
+}
+
+func (t Telemetry) GetIdFromFile() {
+	file, _ := ioutil.ReadFile("telemetry.json")
+
+	_ = json.Unmarshal([]byte(file), &t)
+
+	if t.ID == "" {
+		t.ID = uuid.New().String()
+		t.setIdToFile()
+	}
+}
+
+func (t Telemetry) setIdToFile() {
+	file, _ := json.MarshalIndent(t, "", " ")
+
+	_ = ioutil.WriteFile("telemetry.json", file, 0644)
+}
+
+func (t Telemetry) PostInformation(ctx context.Context, url string, facts map[string]string) {
+	body, _ := json.Marshal(map[string]string{
+		"id":                  t.ID,
+		"cpu_cores":           facts["cpu_cores"],
+		"cpu_model":           facts["cpu_model_name"],
+		"country":             facts["timezone"],
+		"installation_format": facts["installation_format"],
+		"kernel_version":      facts["kernel_major_version"],
+		"memory":              facts["memory"],
+		"product":             "Squirreldb",
+		"os_type":             facts["os_name"],
+		"os_version":          facts["os_version"],
+		"system_architecture": facts["architecture"],
+		"version":             facts["glouton_version"],
+	})
+
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
+
+	req.Header.Set("Content-Type", "application/json")
+
+	ctx2, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx2))
+
+	if err != nil {
+		fmt.Printf("failed when we post on telemetry: %v", err)
+	}
+
+	if resp != nil {
+		fmt.Printf("telemetry response Satus: %s", resp.Status)
+		defer resp.Body.Close()
+	}
+}
