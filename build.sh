82451eeb75580ad2b71de1c1787b4e46691309c5
Pierre Fersing <pierre.fersing@bleemeo.com>
2022-06-13 11:57:57 +0200

    Fix build without squirreldb-buildcache volume

--- build.sh
+++ build.sh
@@ -45,5 +45,5 @@ else
       --entrypoint '' \
       -e GORELEASER_PREVIOUS_TAG=0.1.0 \
       -e GORELEASER_CURRENT_TAG=0.1.1 \
-      goreleaser/goreleaser:${GORELEASER_VERSION} sh -c "(git config --global --add safe.directory /src && goreleaser check && go test ./... && goreleaser --rm-dist --snapshot --parallelism 2);result=\$?;chown -R $USER_UID dist; exit \$result"
+      goreleaser/goreleaser:${GORELEASER_VERSION} sh -c "(mkdir -p /go/pkg && git config --global --add safe.directory /src && goreleaser check && go test ./... && goreleaser --rm-dist --snapshot --parallelism 2);result=\$?;chown -R $USER_UID dist; exit \$result"
 fi
