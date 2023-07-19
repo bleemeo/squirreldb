585a71d343bfc5f3910888b8b219b2a82e734eb8
Thomas Delbende <thomas.delbende@bleemeo.com>
2023-07-19 17:42:43 +0200

    Make metricsFromTimeSeries a method

--- api/remotestorage/write.go
+++ api/remotestorage/write.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"math"
-	"squirreldb/cassandra/tsdb"
 	"squirreldb/types"
 	"time"
 
@@ -15,8 +13,9 @@ import (
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/metadata"
 	"github.com/prometheus/prometheus/storage"
-	"github.com/rs/zerolog/log"
 )
+
+const pointInPastLogPeriod = 10 * time.Second
 
 var (
 	ErrInvalidMatcher = errors.New("invalid labels")
@@ -40,6 +39,14 @@ type writeMetrics struct {
 
 	// Map of pending timeseries indexed by their labels hash.
 	pendingTimeSeries map[uint64]timeSeries
+
+	// Returns a metric list generated from a TimeSeries list.
+	metricsFromTimeSeries func(
+		ctx context.Context,
+		pendingTimeSeries []timeSeries,
+		index types.Index,
+		timeToLiveSeconds int64,
+	) ([]types.MetricData, int, error)
 
 	// Release a spot in the remote write gate. Must be called.
 	done func()
@@ -110,14 +117,14 @@ func (w *writeMetrics) Commit() error {
 		w.done()
 	}()
 
-	// Convert the time series map to a slice, because metricsFromTimeseries
-	// needs to always iterate on it in the same order.
+	// Convert the time series map to a slice, because w.metricsFromTimeSeries
+	// always needs to iterate on it in the same order.
 	pendingTimeSeries := make([]timeSeries, 0, len(w.pendingTimeSeries))
 	for _, ts := range w.pendingTimeSeries {
 		pendingTimeSeries = append(pendingTimeSeries, ts)
 	}
 
-	metrics, totalPoints, err := metricsFromTimeseries(
+	metrics, totalPoints, err := w.metricsFromTimeSeries(
 		context.Background(),
 		pendingTimeSeries,
 		w.index,
@@ -142,85 +149,6 @@ func (w *writeMetrics) Rollback() error {
 	w.done()
 
 	return nil
-}
-
-const pointInPastLogPeriod = 10 * time.Second
-
-var lastLogPointInPastAt time.Time //nolint:gochecknoglobals
-
-// Returns a metric list generated from a TimeSeries list.
-func metricsFromTimeseries(
-	ctx context.Context,
-	pendingTimeSeries []timeSeries,
-	index types.Index,
-	timeToLiveSeconds int64,
-) ([]types.MetricData, int, error) {
-	if len(pendingTimeSeries) == 0 {
-		return nil, 0, nil
-	}
-
-	idToIndex := make(map[types.MetricID]int, len(pendingTimeSeries))
-
-	totalPoints := 0
-	metrics := make([]types.MetricData, 0, len(pendingTimeSeries))
-
-	requests := make([]types.LookupRequest, 0, len(pendingTimeSeries))
-
-	for _, promSeries := range pendingTimeSeries {
-		min := int64(math.MaxInt64)
-		max := int64(math.MinInt64)
-
-		for _, s := range promSeries.Samples {
-			if min > s.Timestamp {
-				min = s.Timestamp
-			}
-
-			if max < s.Timestamp {
-				max = s.Timestamp
-			}
-		}
-
-		if min < time.Now().Add(-tsdb.MaxPastDelay).Unix()*1000 {
-			if lastLogPointInPastAt.Before(time.Now().Add(-pointInPastLogPeriod)) {
-				log.Warn().Msgf("Points with timestamp %v will be ignored by pre-aggregation", time.Unix(min/1000, 0))
-				lastLogPointInPastAt = time.Now()
-			}
-		}
-
-		requests = append(requests, types.LookupRequest{
-			Labels:     promSeries.Labels,
-			TTLSeconds: timeToLiveSeconds,
-			End:        time.Unix(max/1000, max%1000),
-			Start:      time.Unix(min/1000, min%1000),
-		})
-	}
-
-	ids, ttls, err := index.LookupIDs(ctx, requests)
-	if err != nil {
-		return nil, totalPoints, fmt.Errorf("metric ID lookup failed: %w", err)
-	}
-
-	for i, promSeries := range pendingTimeSeries {
-		data := types.MetricData{
-			ID:         ids[i],
-			Points:     promSeries.Samples,
-			TimeToLive: ttls[i],
-		}
-
-		if idx, found := idToIndex[data.ID]; found {
-			metrics[idx].Points = append(metrics[idx].Points, data.Points...)
-			if metrics[idx].TimeToLive < data.TimeToLive {
-				metrics[idx].TimeToLive = data.TimeToLive
-			}
-		} else {
-			metrics = append(metrics, data)
-			idToIndex[data.ID] = len(metrics) - 1
-		}
-
-		totalPoints += len(data.Points)
-	}
-
-	return metrics, totalPoints, nil
 }
 
 // validateLabels checks if the metric name and labels are valid.
